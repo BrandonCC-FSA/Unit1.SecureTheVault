a385d80a95bc5c4481cb8c0d87a53fda705a3fe3
Brandon Corrigall <corrb812@gmail.com>
2025-05-14 15:12:02 -0400

    changed let on line 8 to const

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const code2 = 120 / 2 - 20;
 
 const code3 = 79 % 40;
 
-let message = `The vault has been secured. The combinations is:`;
+const message = `The vault has been secured. The combinations is:`;
 
 const codeA = code1 + `-` + code2 + `-` + code3;
 
